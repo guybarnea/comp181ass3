2d03396eb9fceda4ca6f330ef4e95783f54fec11
Guyb <guybarnea22@gmail.com>
2017-12-16 18:42:31 +0200

    implementation of 'pe->lex-pe'

--- semantic-analyzer.scm
+++ semantic-analyzer.scm
@@ -1,3 +1,7 @@
+(load "tag-parser.scm")
+
+
+
 (define applic-lambda-simple-nil?
   (lambda (exp)
   	(if (and (> (length exp) 1) (list? exp) (eq? (car exp) 'applic) (list? (cadr exp)))
@@ -27,12 +31,119 @@
 	    (else el))) (remove-applic-lambda-nil-tag lst))
 ))  
 
+
+
+(define lambda-args->lst
+  (lambda (el)
+    (let ((lambda-type (car el))
+	  (args (cadr el)))
+    (cond
+      ((equal? lambda-type 'lambda-simple) args)
+      ((equal? lambda-type 'lambda-opt) (append args (list (caddr el))))
+      (else (list args))))   
+))
+
+
+
+(define get-lambda-body
+  (lambda (el)
+    (let ((lambda-type (car el)))
+      (cond
+	((equal? lambda-type 'lambda-opt) (cadddr el))
+	(else (caddr el))))   
+))
+
+
+(define get-lambda-args
+  (lambda (el)
+    (let ((lambda-type (car el))
+	  (args (cadr el)))
+    (cond
+      ((null? args) '(()))
+      ((equal? lambda-type 'lambda-simple) `(,args))
+      ((equal? lambda-type 'lambda-opt) `(,args ,(caddr el)))
+      (else (list args))))   
+))
+
+(define lambda?
+  (lambda (exp)
+    (or 
+      (equal? exp 'lambda-var)
+      (equal? exp 'lambda-simple)
+      (equal? exp 'lambda-opt))
+))  
+
+(define replace-bound
+  (lambda (var major minor exp)
+      (cond
+	((null? exp) exp)
+	((list? exp)
+	    (cond
+	      ((and (= 2 (length exp)) (equal? (car exp) 'var) (equal? (cadr exp) var))
+		`(bvar ,var ,major ,minor))
+	      ((and (lambda? (car exp)) (not (member var (lambda-args->lst exp)))) ; bound
+		`(,(car exp) ,@(get-lambda-args exp) 
+		  ,(replace-bound var (+ 1 major) minor (get-lambda-body exp))))
+	      ((lambda? (car exp)) exp)
+	      (else (map (lambda (x) (replace-bound var major minor x)) exp))))
+	(else exp))
+))	
+
+(define replace-param
+  (lambda (var minor exp)
+      (cond
+	((null? exp) exp)
+	((list? exp)
+	    (cond
+	      ((and (= 2 (length exp)) (equal? (car exp) 'var) (equal? (cadr exp) var))
+		`(pvar ,var ,minor))
+	      ((and (lambda? (car exp)) (not (member var (lambda-args->lst exp)))) ; bound
+		`(,(car exp) ,@(get-lambda-args exp) ,(replace-bound var 0 minor (get-lambda-body exp))))
+	      ((lambda? (car exp)) exp)  	  
+	      (else (map (lambda (x) (replace-param var minor x)) exp))))
+	(else exp))
+))	
+
+(define replace-params
+  (lambda (vars minor exp)
+    (if (null? vars) exp
+	(replace-params (cdr vars) (+ minor 1) (replace-param (car vars) (+ minor 1) exp)))
+))
+
+(define search-param-bound-vars
+    (lambda (exp)
+      (cond
+	((null? exp) exp)
+	((list? exp)
+	    (cond 
+	      ((and (not (null? exp)) (lambda? (car exp)))
+		`(,(car exp) ,@(get-lambda-args exp)
+		  ,(search-param-bound-vars (replace-params 
+		      (lambda-args->lst exp)
+		      -1 
+		      (get-lambda-body exp)))))
+	      (else (map search-param-bound-vars exp))))
+	(else exp))
+))
+
+(define pe->lex-pe
+    (lambda (exp)
+      (letrec ((exp-tag (search-param-bound-vars exp))
+	       (iter (lambda (exp-tag) 
+		      (cond
+			((null? exp-tag) exp-tag)
+			((list? exp-tag)
+			    (cond
+			      ((and (= 2 (length exp-tag)) (equal? (car exp-tag) 'var))
+				`(fvar ,(cadr exp-tag)))	  
+			      (else (map iter exp-tag))))
+			(else exp-tag)))))
+	  (iter exp-tag))
+))
+
+
 ; (define box-set
 ;   ;; fill in the variable boxing details here
-;   )
-
-; (define pe->lex-pe
-;   ;; fill in the lexical addressing details here
 ;   )
 
 ; (define annotate-tc
