33f4a07919bd161d27186a6a9f29968741645d27
Guyb <guybarnea22@gmail.com>
2017-12-16 13:09:14 +0200

    implementation of 'remove-applic-lambda-nil'

--- semantic-analyzer.scm
+++ semantic-analyzer.scm
@@ -1,15 +1,40 @@
+(define applic-lambda-simple-nil?
+  (lambda (exp)
+  	(if (and (> (length exp) 1) (list? exp) (eq? (car exp) 'applic) (list? (cadr exp)))
+  		(let ((expr (cadr exp)))
+  			 (if (and (> (length expr) 2) (eq? (car expr) 'lambda-simple) (null? (cadr expr)))	 
+  			 (caddr expr)
+  			 '()))
+  			 '())))
+
+
+
+
+(define remove-applic-lambda-nil-tag
+	(lambda (exp)
+
+  		(let ((body (applic-lambda-simple-nil? exp)))
+  			(cond
+  			((null? body) exp)
+  			((list? body) (remove-applic-lambda-nil body))
+  			(else body)))))
+
 (define remove-applic-lambda-nil
-  ;; fill in the empty-lambda application elimination details here
-  )
+  (lambda (lst)
+    (map (lambda (el) 
+	    (cond 
+	    ((list? el) (remove-applic-lambda-nil el))
+	    (else el))) (remove-applic-lambda-nil-tag lst))
+))  
 
-(define box-set
-  ;; fill in the variable boxing details here
-  )
+; (define box-set
+;   ;; fill in the variable boxing details here
+;   )
 
-(define pe->lex-pe
-  ;; fill in the lexical addressing details here
-  )
+; (define pe->lex-pe
+;   ;; fill in the lexical addressing details here
+;   )
 
-(define annotate-tc
-  ;; fill in the tail-call annotation details here
-  )
+; (define annotate-tc
+;   ;; fill in the tail-call annotation details here
+;   )
